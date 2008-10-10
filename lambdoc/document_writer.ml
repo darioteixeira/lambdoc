4a6bbee86fd3c0645e8e2c433491355b7e895b5d
darioteixeira <darioteixeira@yahoo.com>
2008-10-10 19:52:30 +0000

    Added document_writer.ml, defining a WRITER module sig that all document writers must conform to

---
+++ lambdoc/document_writer.ml
@@ -0,0 +1,35 @@
+(********************************************************************************)
+(**	Document writer.
+
+	Copyright (c) 2007-2008 Dario Teixeira (dario.teixeira@yahoo.com)
+
+	This software is distributed under the terms of the GNU GPL version 2.
+	See LICENSE file for full license text.
+*)
+(********************************************************************************)
+
+open Document_valid
+open Document_invalid
+open Document_ambivalent
+
+
+(********************************************************************************)
+(*	{2 Public modules}							*)
+(********************************************************************************)
+
+(**	The module type that all wannabe document writers must export.
+*)
+module type WRITER =
+sig
+	type t
+
+	val write_valid_manuscript: Valid.manuscript_t -> t
+	val write_valid_composition: Valid.composition_t -> t
+
+	val write_invalid_manuscript: Invalid.manuscript_t -> t
+	val write_invalid_composition: Invalid.composition_t -> t
+
+	val write_ambivalent_manuscript: Ambivalent.manuscript_t -> t
+	val write_ambivalent_composition: Ambivalent.composition_t -> t
+end
+
