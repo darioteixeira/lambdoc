5b6b69362c5ad90b18f53034efd7cd682a6bf9f3
darioteixeira <darioteixeira@yahoo.com>
2009-01-01 21:37:39 +0000

    Added interface file for Extra module

---
+++ src/lambdoc_reader/extra.mli
@@ -0,0 +1,32 @@
+(********************************************************************************)
+(*	Interface file for Extra module.
+
+	Copyright (c) 2007-2008 Dario Teixeira (dario.teixeira@yahoo.com)
+
+	This software is distributed under the terms of the GNU GPL version 2.
+	See LICENSE file for full license text.
+*)
+(********************************************************************************)
+
+(**	Definition of the extra parameters.
+*)
+
+open Lambdoc_core
+
+
+(********************************************************************************)
+(**	{2 Definition of functions and values}					*)
+(********************************************************************************)
+
+val parse_for_itemize: 		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Bullet.t
+val parse_for_enumerate:	(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Numbering.t
+val parse_floater:		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_quote: 		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_mathtex: 		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_mathml: 		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_code:		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_verbatim:		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_tabular:		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_bitmap:		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+val parse_for_subpage:		(int * Error.error_msg_t) DynArray.t -> Ast.M.command_t -> string option -> Alignment.t
+
