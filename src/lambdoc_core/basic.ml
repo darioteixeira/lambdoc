fc796709cf2a63466b373d42aaf50619bd3ac32c
darioteixeira <darioteixeira@yahoo.com>
2009-01-22 19:03:30 +0000

    Added hooks for bin_prot

--- src/lambdoc_core/basic.ml
+++ src/lambdoc_core/basic.ml
@@ -18,23 +18,23 @@ TYPE_CONV_PATH "Document"
 (**	{2 Type definitions}							*)
 (********************************************************************************)
 
-type raw_t = string with sexp		(** The type of raw text. *)
+type raw_t = string with sexp			(** The type of raw text. *)
 
-type plain_t = string with sexp		(** The type of undecorated text. *)
+type plain_t = string with sexp			(** The type of undecorated text. *)
 
 type entity_t = string with sexp		(** The type of XHTML entities. *)
 
-type link_t = string with sexp		(** The type of links. *)
+type link_t = string with sexp			(** The type of links. *)
 
-type alias_t = string with sexp		(** The type of aliases to filenames. *)
+type alias_t = string with sexp			(** The type of aliases to filenames. *)
 
-type ref_t = string with sexp		(** The type of label references. *)
+type ref_t = string with sexp			(** The type of label references. *)
 
-type syntax_t = string option with sexp	(** The type for syntax declarations. *)
+type syntax_t = string option with sexp		(** The type for syntax declarations. *)
 
-type tag_t = string with sexp		(** The type used for tags. *)
+type tag_t = string with sexp			(** The type used for tags. *)
 
-type 'a plus_t = 'a * 'a list with sexp	(** The type of non-empty lists. *)
+type 'a plus_t = 'a * 'a list with sexp		(** The type of non-empty lists. *)
 
 
 (**     Definition of hierarchy levels for sections.  We support a three-level
