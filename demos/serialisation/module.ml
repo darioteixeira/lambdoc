d73346648c8a8daa0b51671b580712fb70522ba9
darioteixeira <darioteixeira@yahoo.com>
2009-02-26 16:54:33 +0000

    Created serialisation speed test

--- demos/serialisation/module.ml
+++ demos/serialisation/module.ml
@@ -1,44 +1,46 @@
-open XHTML.M
 open Lambdoc_reader
-open Features
 
-(* --------------------------------------------------------------------	*)
-(* Service "coucou".							*)
-(* --------------------------------------------------------------------	*)
 
-let coucou_handler sp () () =
+let doc =
 	let src = IO.read_all (IO.input_channel (open_in "sample.ltex")) in
-	let css_uri = Eliom_predefmod.Xhtml.make_uri (Eliom_services.static_dir sp) sp ["css"; "lambdoc.css"] in
 	let accept_list = [ ] in
 	let deny_list = [ ] in
-	let default : default_t = `Accept in
-	let time1 = Unix.gettimeofday () in
-	let doc = Read_lambtex.Main.ambivalent_manuscript_from_string ~accept_list ~deny_list ~default src in
-	let time2 = Unix.gettimeofday () in
-	let pickle = Lambdoc_core.Ambivalent.serialize_manuscript doc in
-	let time3 = Unix.gettimeofday () in
-	let new_doc = Lambdoc_core.Ambivalent.deserialize_manuscript pickle in
-	let time4 = Unix.gettimeofday () in
-	let xhtml = Write_xhtml.Main.write_ambivalent_manuscript new_doc in
-	let time5 = Unix.gettimeofday () in
-	let () =
-		Ocsigen_messages.warning (Printf.sprintf "Parsing took %.3f secs" (time2 -. time1));
-		Ocsigen_messages.warning (Printf.sprintf "Serialising to S-exp took %.3f secs" (time3 -. time2));
-		Ocsigen_messages.warning (Printf.sprintf "Deserialising from S-exp took %.3f secs" (time4 -. time3));
-		Ocsigen_messages.warning (Printf.sprintf "Writing XHTML took %.3f secs" (time5 -. time4))
-	in Lwt.return
-		(html
-			(head ~a:[a_profile (uri_of_string "http://www.w3.org/2005/11/profile")]
-				(title (pcdata "Lambtex Tutorial"))
-				[
-				meta ~a: [a_http_equiv "content-type"] ~content: "text/html; charset=utf-8" ();
-				Eliom_predefmod.Xhtml.css_link ~a:[(a_media [`All]); (a_title "Default")] ~uri:css_uri ()
-				])
-			(body [xhtml]))
+	let default = `Accept
+	in Read_lambtex.Main.ambivalent_manuscript_from_string ~accept_list ~deny_list ~default src
 
-let coucou_service =
-	Eliom_predefmod.Xhtml.register_new_service 
-		~path: [""]
-		~get_params: Eliom_parameters.unit
-		coucou_handler
+
+let sexp_pickle = Lambdoc_core.Ambivalent.serialize_manuscript_to_sexp doc
+let binprot_pickle = Lambdoc_core.Ambivalent.serialize_manuscript_to_binprot doc
+let marshal_pickle = Marshal.to_string doc []
+
+let conv_to_sexp () = Lambdoc_core.Ambivalent.serialize_manuscript_to_sexp doc
+let conv_from_sexp () = Lambdoc_core.Ambivalent.deserialize_manuscript_from_sexp sexp_pickle
+
+let conv_to_binprot () = Lambdoc_core.Ambivalent.serialize_manuscript_to_binprot doc
+let conv_from_binprot () = Lambdoc_core.Ambivalent.deserialize_manuscript_from_binprot binprot_pickle
+
+let conv_to_marshal () = Marshal.to_string doc []
+let conv_from_marshal () = Marshal.from_string marshal_pickle 0
+
+
+let benchmark func =
+	let time1 = Unix.gettimeofday () in
+	let () = for i = 1 to 100000 do func () done in
+	let time2 = Unix.gettimeofday () in
+	time2 -. time1
+
+
+let tests =
+	let t1 = benchmark conv_to_sexp in
+	let t2 = benchmark conv_from_sexp in
+	let t3 = benchmark conv_to_binprot in
+	let t4 = benchmark conv_from_binprot in
+	let t5 = benchmark conv_to_marshal in
+	let t6 = benchmark conv_from_marshal
+	in	Printf.printf "Serialisation with Sexp took %.3f secs\n" t1;
+		Printf.printf "Deserialisation with Sexp took %.3f secs\n" t2;
+		Printf.printf "Serialisation with Binprot took %.3f secs\n" t3;
+		Printf.printf "Deserialisation with Binprot took %.3f secs\n" t4;
+		Printf.printf "Serialisation with Marshal took %.3f secs\n" t5;
+		Printf.printf "Deserialisation with Marshal took %.3f secs\n" t6
 
