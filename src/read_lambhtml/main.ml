74ad16771d988b043f7ef8249ca21787eb4d25d9
darioteixeira <darioteixeira@yahoo.com>
2009-07-14 12:10:34 +0000

    Better Lambhtml coverage

--- src/read_lambhtml/main.ml
+++ src/read_lambhtml/main.ml
@@ -35,44 +35,49 @@ struct
 		comm_linenum = let (_, line, _) = node#position in line;
 		}
 
-	let rec ast_from_seq seq =
-		List.map ast_from_inline_node seq
+	let rec process_seq seq =
+		List.map process_inline seq
 
-	and ast_from_inline_node node =
+	and process_inline node =
 		let comm = lazy (command_from_node node)
 		in match node#node_type with
 			| T_data			-> (!!comm, Ast.Plain node#data)
+			| T_element "tex"		-> (!!comm, Ast.Mathtex_inl node#data)
+			| T_element "math"		-> (!!comm, Ast.Mathml_inl node#data)
 			| T_element "bold"
 			| T_element "strong"
-			| T_element "b"			-> (!!comm, Ast.Bold (ast_from_seq node#sub_nodes))
+			| T_element "b"			-> (!!comm, Ast.Bold (process_seq node#sub_nodes))
 			| T_element "emph"
 			| T_element "em"
-			| T_element "i"			-> (!!comm, Ast.Emph (ast_from_seq node#sub_nodes))
-			| T_element "mono"		-> (!!comm, Ast.Mono (ast_from_seq node#sub_nodes))
-			| T_element "caps"		-> (!!comm, Ast.Caps (ast_from_seq node#sub_nodes))
-			| T_element "thru"		-> (!!comm, Ast.Thru (ast_from_seq node#sub_nodes))
-			| T_element "sup"		-> (!!comm, Ast.Sup (ast_from_seq node#sub_nodes))
-			| T_element "sub"		-> (!!comm, Ast.Sub (ast_from_seq node#sub_nodes))
-			| T_element "mbox"		-> (!!comm, Ast.Mbox (ast_from_seq node#sub_nodes))
+			| T_element "i"			-> (!!comm, Ast.Emph (process_seq node#sub_nodes))
+			| T_element "mono"		-> (!!comm, Ast.Mono (process_seq node#sub_nodes))
+			| T_element "caps"		-> (!!comm, Ast.Caps (process_seq node#sub_nodes))
+			| T_element "thru"		-> (!!comm, Ast.Thru (process_seq node#sub_nodes))
+			| T_element "sup"		-> (!!comm, Ast.Sup (process_seq node#sub_nodes))
+			| T_element "sub"		-> (!!comm, Ast.Sub (process_seq node#sub_nodes))
+			| T_element "mbox"		-> (!!comm, Ast.Mbox (process_seq node#sub_nodes))
 			| T_element "link"
-			| T_element "a"			-> (!!comm, Ast.Link (node#required_string_attribute "href", ast_from_seq node#sub_nodes))
+			| T_element "a"			-> (!!comm, Ast.Link (node#required_string_attribute "href", process_seq node#sub_nodes))
 			| T_element "see"		-> (!!comm, Ast.See (node#required_string_attribute "href"))
 			| T_element "cite"		-> (!!comm, Ast.Cite (node#required_string_attribute "href"))
 			| T_element "ref"		-> (!!comm, Ast.Ref (node#required_string_attribute "href"))
 			| T_element "sref"		-> (!!comm, Ast.Sref (node#required_string_attribute "href"))
-			| T_element "mref"		-> (!!comm, Ast.Mref (node#required_string_attribute "href", ast_from_seq node#sub_nodes))
-			| _				-> failwith "ast_from_inline_node"
+			| T_element "mref"		-> (!!comm, Ast.Mref (node#required_string_attribute "href", process_seq node#sub_nodes))
+			| _				-> failwith "process_inline_node"
 
-	let ast_from_block_node node =
+	let rec process_frag frag =
+		List.map process_block frag
+
+	and process_block node =
 		let comm = lazy (command_from_node node)
 		in match node#node_type with
-			| T_element "p"			-> (!!comm, Ast.Paragraph (ast_from_seq node#sub_nodes))
+			| T_element "p"			-> (!!comm, Ast.Paragraph (process_seq node#sub_nodes))
 			| T_element "itemize"
-			| T_element "ul"		-> (!!comm, Ast.Itemize [])
+			| T_element "ul"		-> (!!comm, Ast.Itemize (List.map process_item node#sub_nodes))
 			| T_element "enumerate"
-			| T_element "ol"		-> (!!comm, Ast.Enumerate [])
+			| T_element "ol"		-> (!!comm, Ast.Enumerate (List.map process_item node#sub_nodes))
 			| T_element "description"
-			| T_element "dl"		-> (!!comm, Ast.Description [])
+			| T_element "dl"		-> (!!comm, Ast.Description (process_definition_frag node#sub_nodes))
 			| T_element "quote"		-> (!!comm, Ast.Quote [])
 			| T_element "callout"		-> (!!comm, Ast.Callout (None, []))
 			| T_element "code"		-> (!!comm, Ast.Code "")
@@ -80,17 +85,18 @@ struct
 			| T_element "verbatim"		-> (!!comm, Ast.Verbatim "")
 			| T_element "bitmap"		-> (!!comm, Ast.Bitmap ("", ""))
 			| T_element "subpage"		-> (!!comm, Ast.Subpage [])
-			(*
-			| T_element "equation"		-> (!!comm, Ast.Equation)
-			| T_element "printout"		-> (!!comm, Ast.Printout)
-			| T_element "table"		-> (!!comm, Ast.Table)
-			| T_element "figure"		-> (!!comm, Ast.Figure)
-			*)
+			| T_element "equation"		-> let (block, caption) = process_wrapper node in (!!comm, Ast.Equation (caption, block))
+			| T_element "printout"		-> let (block, caption) = process_wrapper node in (!!comm, Ast.Printout (caption, block))
+			| T_element "table"		-> let (block, caption) = process_wrapper node in (!!comm, Ast.Table (caption, block))
+			| T_element "figure"		-> let (block, caption) = process_wrapper node in (!!comm, Ast.Figure (caption, block))
 			| T_element "part"		-> (!!comm, Ast.Part [])
 			| T_element "appendix"		-> (!!comm, Ast.Appendix)
-			| T_element "section"		-> (!!comm, Ast.Section (`Level1, []))
-			| T_element "subsection"	-> (!!comm, Ast.Section (`Level2, []))
-			| T_element "subsubsection"	-> (!!comm, Ast.Section (`Level3, []))
+			| T_element "section"
+			| T_element "h1"		-> (!!comm, Ast.Section (`Level1, []))
+			| T_element "subsection"
+			| T_element "h2"		-> (!!comm, Ast.Section (`Level2, []))
+			| T_element "subsubsection"
+			| T_element "h3"		-> (!!comm, Ast.Section (`Level3, []))
 			| T_element "bibliography"	-> (!!comm, Ast.Bibliography)
 			| T_element "notes"		-> (!!comm, Ast.Notes)
 			| T_element "toc"		-> (!!comm, Ast.Toc)
@@ -98,14 +104,53 @@ struct
 			| T_element "subtitle"		-> (!!comm, Ast.Title (`Level2, []))
 			| T_element "abstract"		-> (!!comm, Ast.Abstract [])
 			| T_element "rule"		-> (!!comm, Ast.Rule)
-			(*
-			| T_element "bib"		-> (!!comm, Ast.Bib)
-			*)
+			| T_element "bib"		-> let (who, what, where) = process_bib node#sub_nodes in (!!comm, Ast.Bib (who, what, where))
 			| T_element "note"		-> (!!comm, Ast.Note [])
-			| _				-> failwith "ast_from_block_node"
+			| _				-> failwith "process_block_node"
 
-	let ast_from_document node = match node#node_type with
-		| T_element "document" -> List.map ast_from_block_node node#sub_nodes
+	and process_item node =
+		let comm = lazy (command_from_node node)
+		in match node#node_type with
+			| T_element "li"		-> (!!comm, process_frag node#sub_nodes)
+			| _				-> failwith "process_item_node"
+
+	and process_definition_frag frag =
+
+		let pairify frag =
+			let rec pairify_aux accum = function
+				| dt::dd::tl	-> let new_accum = (dt, dd) :: accum in pairify_aux new_accum tl
+				| []		-> accum
+				| _		-> failwith "pairify"
+			in pairify_aux [] frag in
+
+		let process_definition_nodes (dt_node, dd_node) =
+			let comm = lazy (command_from_node dt_node) in
+			let (dt, dd) = match (dt_node#node_type, dd_node#node_type) with
+				| (T_element "dt", T_element "dd")	-> (process_seq dt_node#sub_nodes, process_frag dd_node#sub_nodes)
+				| _					-> failwith "process_definition_nodes"
+			in (!!comm, dt, dd)
+
+		in List.rev_map process_definition_nodes (pairify frag)
+
+	and process_wrapper node = match node#sub_nodes with
+		| [block_node; caption_node] when caption_node#node_type = T_element "caption" ->
+			let block = process_block block_node
+			and caption = (command_from_node caption_node, process_seq caption_node#sub_nodes)
+			in (block, caption)
+		| _ ->
+			failwith "process_wrapper"
+
+	and process_bib = function
+		| [who_node; what_node; where_node] ->
+			let who = (command_from_node who_node, process_seq who_node#sub_nodes)
+			and what = (command_from_node what_node, process_seq what_node#sub_nodes)
+			and where = (command_from_node where_node, process_seq where_node#sub_nodes)
+			in (who, what, where)
+		| _ ->
+			failwith "process_bib"
+
+	let process_document node = match node#node_type with
+		| T_element "document" -> process_frag node#sub_nodes
 		| _			-> failwith "unknown"
 
 	let dtd =
@@ -118,7 +163,7 @@ struct
 		let spec = Pxp_tree_parser.default_spec in
 		let source = Pxp_types.from_string ("<document>" ^ str ^ "</document>") in
 		let tree = Pxp_tree_parser.parse_content_entity config source dtd spec
-		in ast_from_document tree#root
+		in process_document tree#root
 end
 
 module M = Reader.Make_reader (R)
