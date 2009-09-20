3a0d0c528c257a06e71599f19ce2cf92f702561a
darioteixeira <darioteixeira@yahoo.com>
2009-09-20 22:43:28 +0000

    Proxy now reports communication errors

--- src/lambdoc_proxy/client.ml
+++ src/lambdoc_proxy/client.ml
@@ -12,21 +12,33 @@ open Lambdoc_core
 
 
 (********************************************************************************)
+(*	{2 Exceptions}								*)
+(********************************************************************************)
+
+exception Proxy_communication_error
+
+
+(********************************************************************************)
 (*	{2 Private functions and values}					*)
 (********************************************************************************)
 
 let communicate request =
-        let addr = Unix.ADDR_INET (Unix.inet_addr_loopback, 9999)
-        and sock = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-        Lwt_unix.connect sock addr >>= fun () ->
-        let in_channel = Lwt_chan.in_channel_of_descr sock
-        and out_channel = Lwt_chan.out_channel_of_descr sock in
-        Lwt_chan.output_value out_channel request >>= fun () ->
-        Lwt_chan.flush out_channel >>= fun () ->
-        Lwt_chan.input_value in_channel >>= fun reply ->
-        Lwt_unix.shutdown sock Unix.SHUTDOWN_ALL;
-        Lwt_unix.close sock;
-        Lwt.return reply
+	Lwt.catch
+		(fun () ->
+			let addr = Unix.ADDR_INET (Unix.inet_addr_loopback, 9999)
+			and sock = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+			Lwt_unix.connect sock addr >>= fun () ->
+			let in_channel = Lwt_chan.in_channel_of_descr sock
+			and out_channel = Lwt_chan.out_channel_of_descr sock in
+			Lwt_chan.output_value out_channel request >>= fun () ->
+			Lwt_chan.flush out_channel >>= fun () ->
+			Lwt_chan.input_value in_channel >>= fun reply ->
+			Lwt_unix.shutdown sock Unix.SHUTDOWN_ALL;
+			Lwt_unix.close sock;
+			Lwt.return reply)
+		(function
+			| Unix.Unix_error _ -> Lwt.fail Proxy_communication_error
+			| exc		    -> Lwt.fail exc)
 
 
 (********************************************************************************)
