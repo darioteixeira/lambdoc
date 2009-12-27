89c23ffc800cc25af6d1e730c46b92e2ccd96c5b
darioteixeira <darioteixeira@yahoo.com>
2009-12-27 19:10:15 +0000

    Updated Proxy demo

--- demos/proxy/test.ml
+++ demos/proxy/test.ml
@@ -1,24 +1,18 @@
-(********************************************************************************)
-(********************************************************************************)
-
 open Lwt
 open XHTML.M
+open Lambdoc_proxy
 
-(* -------------------------------------------------------------------- *)
-(* Service "coucou".                                                    *)
-(* -------------------------------------------------------------------- *)
-
-let coucou_handler sp () () =
+let test_handler sp () () =
 	let chan = open_in "sample.ltex" in
 	let inp = IO.input_channel chan in
-        let src = IO.read_all inp in
+	let src = IO.read_all inp in
 	let () = IO.close_in inp in
 	let () = close_in chan in
-        let css_uri = Eliom_predefmod.Xhtml.make_uri (Eliom_services.static_dir sp) sp ["css"; "lambdoc.css"] in
-        Lambdoc_proxy.Client.ambivalent_manuscript_from_string `Lambtex src >>= fun doc ->
-        let xhtml = Lambdoc_write_xhtml.Main.write_ambivalent_manuscript doc in
-        Lwt.return
-                (html
+	let css_uri = Eliom_predefmod.Xhtml.make_uri (Eliom_services.static_dir sp) sp ["css"; "lambdoc.css"] in
+	Client.ambivalent_manuscript_from_string src Protocol.Lambtex >>= fun doc ->
+	let xhtml = Lambdoc_write_xhtml.Main.write_ambivalent_manuscript doc in
+	Lwt.return
+		(html
                         (head ~a:[a_profile (uri_of_string "http://www.w3.org/2005/11/profile")]
                                 (title (pcdata "Lambtex Tutorial"))
                                 [
@@ -27,9 +21,9 @@ let coucou_handler sp () () =
                                 ])
                         (body [xhtml]))
 
-let coucou_service =
+let test_service =
         Eliom_predefmod.Xhtml.register_new_service
                 ~path: [""]
                 ~get_params: Eliom_parameters.unit
-                coucou_handler
+                test_handler
 
