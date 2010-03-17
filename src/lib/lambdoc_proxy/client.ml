97235d89bfc413d86bdf6ed7310d6b0b493281b3
darioteixeira <darioteixeira@yahoo.com>
2010-03-17 14:57:09 +0000

    Proxy client now configurable and defined via a functorial interface

--- src/lib/lambdoc_proxy/client.ml
+++ src/lib/lambdoc_proxy/client.ml
@@ -12,65 +12,102 @@ open Lambdoc_core
 
 
 (********************************************************************************)
-(*	{2 Exceptions}								*)
+(**	{1 SOCKET module type}							*)
 (********************************************************************************)
 
-exception Proxy_communication_error
-
-
-(********************************************************************************)
-(*	{2 Private functions and values}					*)
-(********************************************************************************)
-
-let communicate request =
-	Lwt.catch
-		(fun () ->
-			let addr = Unix.ADDR_INET (Unix.inet_addr_loopback, 9999)
-			and sock = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
-			Lwt_unix.connect sock addr >>= fun () ->
-			let in_channel = Lwt_chan.in_channel_of_descr sock
-			and out_channel = Lwt_chan.out_channel_of_descr sock in
-			Lwt_chan.output_value out_channel request >>= fun () ->
-			Lwt_chan.flush out_channel >>= fun () ->
-			Lwt_chan.input_value in_channel >>= fun reply ->
-			Lwt_unix.shutdown sock Unix.SHUTDOWN_ALL;
-			Lwt_unix.close sock;
-			Lwt.return reply)
-		(function
-			| Unix.Unix_error _ -> Lwt.fail Proxy_communication_error
-			| exc		    -> Lwt.fail exc)
+(**	This is the signature that any module given as parameter to the
+	{!Make} functor should obey.  It basically defines the address
+	and properties of the socket that we will create to communicate
+	with the parsing server.
+*)
+module type SOCKET =
+sig
+	val socket_addr: Unix.sockaddr
+	val socket_domain: Unix.socket_domain
+	val socket_type: Unix.socket_type
+	val socket_proto: int
+end
 
 
 (********************************************************************************)
-(*	{2 Public functions and values}						*)
+(**	{1 S module type}							*)
 (********************************************************************************)
 
-let ambivalent_manuscript_from_string ?verify_utf8 ?expand_entities ?accept_list ?deny_list ?default markup source =
-	let payload =
-		{
-		m_verify_utf8 = verify_utf8;
-		m_expand_entities = expand_entities;
-		m_accept_list = accept_list;
-		m_deny_list = deny_list;
-		m_default = default;
-		m_markup = markup;
-		m_source = source;
-		} in
-	communicate (Read_manuscript payload) >>= fun (reply : Ambivalent.manuscript_t) ->
-	Lwt.return reply
+(**	Signature of the module produced by the functor.
+*)
+module type S =
+sig
+	val ambivalent_manuscript_from_string:
+		?verify_utf8: bool ->
+		?expand_entities: bool ->
+		?accept_list: Features.manuscript_feature_t list ->
+		?deny_list: Features.manuscript_feature_t list ->
+		?default: Features.default_t ->
+		Protocol.markup_t ->
+		string ->
+		Lambdoc_core.Ambivalent.manuscript_t Lwt.t
+
+	val ambivalent_composition_from_string:
+		?verify_utf8: bool ->
+		?expand_entities: bool ->
+		?accept_list: Features.composition_feature_t list ->
+		?deny_list: Features.composition_feature_t list ->
+		?default: Features.default_t ->
+		Protocol.markup_t ->
+		string ->
+		Lambdoc_core.Ambivalent.composition_t Lwt.t
+end
 
 
-let ambivalent_composition_from_string ?verify_utf8 ?expand_entities ?accept_list ?deny_list ?default markup source =
-	let payload =
-		{
-		c_verify_utf8 = verify_utf8;
-		c_expand_entities = expand_entities;
-		c_accept_list = accept_list;
-		c_deny_list = deny_list;
-		c_default = default;
-		c_markup = markup;
-		c_source = source;
-		} in
-	communicate (Read_composition payload) >>= fun (reply : Ambivalent.composition_t) ->
-	Lwt.return reply
+(********************************************************************************)
+(**	{1 Make module}								*)
+(********************************************************************************)
+
+(**	Functor that creates an instantiation of a proxy client.
+*)
+module Make (Socket: SOCKET): S =
+struct
+	let communicate request =
+		let addr = Socket.socket_addr
+		and sock = Lwt_unix.socket Socket.socket_domain Socket.socket_type Socket.socket_proto in
+		Lwt_unix.connect sock addr >>= fun () ->
+		let in_channel = Lwt_chan.in_channel_of_descr sock
+		and out_channel = Lwt_chan.out_channel_of_descr sock in
+		Lwt_chan.output_value out_channel request >>= fun () ->
+		Lwt_chan.flush out_channel >>= fun () ->
+		Lwt_chan.input_value in_channel >>= fun reply ->
+		Lwt_unix.shutdown sock Unix.SHUTDOWN_ALL;
+		Lwt_unix.close sock;
+		Lwt.return reply
+
+
+	let ambivalent_manuscript_from_string ?verify_utf8 ?expand_entities ?accept_list ?deny_list ?default markup source =
+		let payload =
+			{
+			m_verify_utf8 = verify_utf8;
+			m_expand_entities = expand_entities;
+			m_accept_list = accept_list;
+			m_deny_list = deny_list;
+			m_default = default;
+			m_markup = markup;
+			m_source = source;
+			} in
+		communicate (Read_manuscript payload) >>= fun (reply : Ambivalent.manuscript_t) ->
+		Lwt.return reply
+
+
+	let ambivalent_composition_from_string ?verify_utf8 ?expand_entities ?accept_list ?deny_list ?default markup source =
+		let payload =
+			{
+			c_verify_utf8 = verify_utf8;
+			c_expand_entities = expand_entities;
+			c_accept_list = accept_list;
+			c_deny_list = deny_list;
+			c_default = default;
+			c_markup = markup;
+			c_source = source;
+			} in
+		communicate (Read_composition payload) >>= fun (reply : Ambivalent.composition_t) ->
+		Lwt.return reply
+end
 
