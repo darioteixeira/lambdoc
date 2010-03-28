84b77c84eb0cab8fba56530962e9585f62cec74e
darioteixeira <darioteixeira@yahoo.com>
2010-03-28 20:41:32 +0000

    Simplified Markup module

--- src/lib/lambdoc_proxy/markup.ml
+++ src/lib/lambdoc_proxy/markup.ml
@@ -8,48 +8,11 @@
 
 
 (********************************************************************************)
-(**	{1 Module signatures}							*)
+(**	{1 Type definitions}							*)
 (********************************************************************************)
 
-module type S =
-sig
-	type t =
-		| Lambtex
-		| Lambhtml
-		| Lamblite
-
-	val of_string: string -> t
-	val to_string: t -> string
-end
-
-
-(********************************************************************************)
-(**	{1 Modules}								*)
-(********************************************************************************)
-
-module M : S =
-struct
-	type t =
-		| Lambtex
-		| Lambhtml
-		| Lamblite
-
-	let of_string = function
-		| "lambtex"  -> Lambtex
-		| "lambhtml" -> Lambhtml
-		| "lamblite" -> Lamblite
-		| _	     -> invalid_arg "Markup.of_string"
-
-	let to_string = function
-		| Lambtex  -> "lambtex"
-		| Lambhtml -> "lambhtml"
-		| Lamblite -> "lamblite"
-end
-
-
-(********************************************************************************)
-(**	{1 Main}								*)
-(********************************************************************************)
-
-include M	(* For convenience sake we include the defined module *)
+type t =
+	| Lambtex
+	| Lambhtml
+	| Lamblite
 
