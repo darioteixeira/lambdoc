d451ca7209d2716d8a099e0a209b4c7ff6613ea0
darioteixeira <darioteixeira@yahoo.com>
2010-03-31 11:51:20 +0000

    Parserver now logging by default

--- src/parserver/parserver.ml
+++ src/parserver/parserver.ml
@@ -6,7 +6,11 @@
 *)
 (********************************************************************************)
 
-let main () =
+let () =
+	Netplex_log.debug_scheduling := false;
+	Netplex_log.debug_containers := true;
+	Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
+	Blahcaml.init_dtd ();
 	let (opt_list, cmdline_cfg) = Netplex_main.args () in
 	let () = Arg.parse opt_list (fun s -> raise (Arg.Bad ("Don't know what to do with: " ^ s))) "usage: netplex [options]" in
 	let parallelizer = Netplex_mp.mp ()
@@ -16,10 +20,4 @@ let main () =
 		Netplex_workload.workload_manager_factories
 		[new Converter.processor_factory]
 		cmdline_cfg
-
-let () =
-	Netplex_log.debug_scheduling := false;
-	Netplex_log.debug_containers := false;
-	Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
-	main ()
 
