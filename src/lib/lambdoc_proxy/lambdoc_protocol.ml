5a785c0c24650dc6e828f97da419fe525d2d6ce1
darioteixeira <darioteixeira@yahoo.com>
2011-10-24 16:46:46 +0000

    Renaming: 'Protocol' -> 'Lambdoc_protocol'

--- src/lib/lambdoc_proxy/lambdoc_protocol.ml
+++ src/lib/lambdoc_proxy/lambdoc_protocol.ml
@@ -1,5 +1,5 @@
 (********************************************************************************)
-(*	Protocol.ml
+(*	Lambdoc_protocol.ml
 	Copyright (c) 2009-2010 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
