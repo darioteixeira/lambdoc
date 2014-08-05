2680454885f99c1af89618266ebe98719e9f38eb
darioteixeira <darioteixeira@yahoo.com>
2014-08-05 15:32:33 +0100

    Update README and copyright date

--- src/lib/lambdoc_reader/ast.ml
+++ src/lib/lambdoc_reader/ast.ml
@@ -1,6 +1,6 @@
 (********************************************************************************)
 (*	Ast.ml
-	Copyright (c) 2009-2013 Dario Teixeira (dario.teixeira@yahoo.com)
+	Copyright (c) 2009-2014 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
 *)
