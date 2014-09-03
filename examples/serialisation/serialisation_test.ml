4da9c2dd4d537cd50060a81f851967507ab119c1
darioteixeira <darioteixeira@yahoo.com>
2014-09-03 18:42:43 +0100

    Port the 'serialisation' example to the current Lambdoc API

---
+++ examples/serialisation/serialisation_test.ml
@@ -0,0 +1,48 @@
+(********************************************************************************)
+(*	Serialisation_test.ml
+	Copyright (c) 2009-2014 Dario Teixeira (dario.teixeira@yahoo.com)
+	This software is distributed under the terms of the GNU GPL version 2.
+	See LICENSE file for full license text.
+*)
+(********************************************************************************)
+
+open Sexplib.Std
+open Lambdoc_reader
+
+
+(********************************************************************************)
+(**	{1 Functions and values}						*)
+(********************************************************************************)
+
+let doc =
+	let src = BatIO.read_all (BatIO.input_channel (open_in "sample.lambtex")) in
+	Lambdoc_read_lambtex.Simple.ambivalent_from_string src
+
+
+let sexp_pickle = Lambdoc_core.Ambivalent.serialize sexp_of_unit sexp_of_unit sexp_of_unit doc
+let marshal_pickle = Marshal.to_string doc []
+
+let conv_to_sexp () = let _ = Lambdoc_core.Ambivalent.serialize sexp_of_unit sexp_of_unit sexp_of_unit doc in ()
+let conv_from_sexp () = let _ = Lambdoc_core.Ambivalent.deserialize unit_of_sexp unit_of_sexp unit_of_sexp sexp_pickle in ()
+
+let conv_to_marshal () = let _ = Marshal.to_string doc [] in ()
+let conv_from_marshal () = let _ = Marshal.from_string marshal_pickle 0 in ()
+
+
+let tests =
+	let funcs =
+		[
+		(conv_to_sexp, "serialisation with Sexp");
+		(conv_from_sexp, "deserialisation with Sexp");
+		(conv_to_marshal, "serialisation with Marshal");
+		(conv_from_marshal, "deserialisation with Marshal")
+		] in
+	let test (func, msg) =
+		Printf.printf "Testing 1000-fold %s... %!" msg;
+		let () = Gc.compact () in
+		let time1 = Unix.gettimeofday () in
+		let () = for i = 1 to 1000 do func () done in
+		let time2 = Unix.gettimeofday () in
+		Printf.printf "Required %.3f secs\n%!" (time2 -. time1)
+	in List.iter test funcs
+
