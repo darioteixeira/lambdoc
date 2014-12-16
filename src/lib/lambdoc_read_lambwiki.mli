d2cf7565ea0c567b51a2d9c2c66d835d849a26e5
darioteixeira <darioteixeira@yahoo.com>
2014-12-16 11:48:03 +0000

    Fix sed snafu

--- src/lib/lambdoc_read_lambwiki.mli
+++ src/lib/lambdoc_read_lambwiki.mli
@@ -2,7 +2,7 @@
 (*	Lambdoc_read_lambwiki.mli
 	Copyright (c) 2009-2014 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
-	See LICENSE file for full license wikit.
+	See LICENSE file for full license text.
 *)
 (********************************************************************************)
 
