9190a412e2e2b0ffb4055fa051b9a9ac960e1a51
darioteixeira <darioteixeira@yahoo.com>
2015-03-25 19:11:36 +0000

    Update copyright date and info

--- src/lib/lambdoc_reader_permission.mli
+++ src/lib/lambdoc_reader_permission.mli
@@ -1,6 +1,6 @@
 (********************************************************************************)
 (*	Lambdoc_reader_permission.mli
-	Copyright (c) 2009-2014 Dario Teixeira (dario.teixeira@yahoo.com)
+	Copyright (c) 2009-2015 Dario Teixeira <dario.teixeira@nleyten.com>
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
 *)
