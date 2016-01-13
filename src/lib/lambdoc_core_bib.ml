65fed3c20c6a7dfb079687afe1957f1e6afc5bb3
darioteixeira <darioteixeira@yahoo.com>
2016-01-13 13:36:35 +0000

    Update copyright year in headers

--- src/lib/lambdoc_core_bib.ml
+++ src/lib/lambdoc_core_bib.ml
@@ -1,6 +1,6 @@
 (********************************************************************************)
 (*  Lambdoc_core_bib.ml
-    Copyright (c) 2009-2015 Dario Teixeira <dario.teixeira@nleyten.com>
+    Copyright (c) 2009-2016 Dario Teixeira <dario.teixeira@nleyten.com>
     This software is distributed under the terms of the GNU GPL version 2.
     See LICENSE file for full license text.
 *)
