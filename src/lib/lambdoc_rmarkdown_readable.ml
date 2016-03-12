0c2b88505364fb95ee3925102f3711311c99a4cf
darioteixeira <darioteixeira@yahoo.com>
2016-03-12 12:36:04 +0000

    Improve Markdown support (via Omd)

--- src/lib/lambdoc_rmarkdown_readable.ml
+++ src/lib/lambdoc_rmarkdown_readable.ml
@@ -15,7 +15,9 @@ open Ast
 (*  {1 Private exceptions}                                                      *)
 (********************************************************************************)
 
-exception Unsupported_feature of string
+exception Unsupported_raw of string
+exception Unsupported_block of Omd.element
+exception Unsupported_inline of Omd.element
 
 
 (********************************************************************************)
@@ -24,6 +26,7 @@ exception Unsupported_feature of string
 
 type context =
     | Block of Ast.block
+    | Multiblock of Ast.block list
     | Inline of Ast.inline
     | Drop
 
@@ -43,6 +46,15 @@ let dummy =
     }
 
 
+let get_entity =
+    let rex = Re.(compile (seq [bos; char '&'; group (rep1 any); char ';'; eos])) in
+    fun str ->
+        let groups = Re.exec rex str in
+        if Re.test groups 1
+        then Some (Re.get groups 1)
+        else None
+                                 
+
 let add_style key = function
     | "" -> dummy
     | v  -> {dummy with comm_style = Some (key ^ "=" ^ v)}
@@ -58,10 +70,11 @@ let blockify xs =
         match fused with
             | [] ->
                 begin match rest with
-                    | []              -> List.rev accum
-                    | Block blk :: tl -> aux (blk :: accum) tl
-                    | Drop :: tl      -> aux accum tl
-                    | Inline _ :: _   -> assert false
+                    | []                    -> List.rev accum
+                    | Block blk :: tl       -> aux (blk :: accum) tl
+                    | Multiblock blks :: tl -> aux (blks @ accum) tl
+                    | Drop :: tl            -> aux accum tl
+                    | Inline _ :: _         -> assert false
                 end
             | fused ->
                 aux ((dummy, Ast.Paragraph fused) :: accum) rest in
@@ -70,10 +83,10 @@ let blockify xs =
 
 let inlinify xs =
     let rec aux accum = function
-        | []               -> List.rev accum
-        | Inline inl :: tl -> aux (inl :: accum) tl
-        | Drop :: tl       -> aux accum tl
-        | Block _ :: _     -> assert false in
+        | []                            -> List.rev accum
+        | Inline inl :: tl              -> aux (inl :: accum) tl
+        | Drop :: tl                    -> aux accum tl
+        | (Block _ | Multiblock _) :: _ -> assert false in
     aux [] xs
 
 
@@ -82,52 +95,94 @@ let ast_of_omd frag =
     let rec convert_seq seq = inlinify (List.map convert_inline seq)
 
     and convert_inline = function
-        | Omd.Text txt           -> Inline (dummy, Ast.Plain txt)
-        | Omd.Emph seq           -> Inline (dummy, Ast.Emph (convert_seq seq))
-        | Omd.Bold seq           -> Inline (dummy, Ast.Bold (convert_seq seq))
-        | Omd.Code (lang, txt)   -> Inline (add_style "lang" lang, Ast.Code txt)
-        | Omd.Br                 -> Inline (dummy, Ast.Linebreak)
-        | Omd.NL                 -> Inline (dummy, Ast.Plain " ")
-        | Omd.Url (href, seq, _) -> Inline (dummy, Ast.Link (href, Some (convert_seq seq)))
+        | Omd.Text txt ->
+            Inline (dummy, Ast.Plain txt)
+        | Omd.Emph seq ->
+            Inline (dummy, Ast.Emph (convert_seq seq))
+        | Omd.Bold seq ->
+            Inline (dummy, Ast.Bold (convert_seq seq))
+        | Omd.Code (lang, txt) ->
+            Inline (add_style "lang" lang, Ast.Code txt)
+        | Omd.Br ->
+            Inline (dummy, Ast.Linebreak)
+        | Omd.NL ->
+            Inline (dummy, Ast.Plain " ")
+        | Omd.Url (href, seq, _) ->
+            Inline (dummy, Ast.Link (href, Some (convert_seq seq)))
         | Omd.Ref (ref, link, txt, fallback) ->
             begin match ref#get_ref link with
                 | Some (href, _title) -> Inline (dummy, Ast.Link ( href, Some ([(dummy, Ast.Plain txt)])))
                 | None                -> Inline (dummy, Ast.Plain fallback#to_string)
             end
-        | _ -> assert false
+        | Omd.Raw raw ->
+            begin match get_entity raw with
+                | Some ent -> Inline (dummy, Ast.Entity ent)
+                | None     -> raise (Unsupported_raw raw)
+            end
+        | Omd.Img (alt, src, _title) ->
+            Inline (dummy, Ast.Glyph (src, alt))
+        | x ->
+            raise (Unsupported_inline x)
+
+    and convert_paragraph accum seq = function
+        | [] ->
+            begin match seq with
+                | [] -> accum
+                | seq -> (dummy, Ast.Paragraph (convert_seq (List.rev seq))) :: accum
+            end
+        | Omd.Hr :: tl ->
+            let rule = (dummy, Ast.Rule) in
+            let accum = match seq with
+                | []  -> rule :: accum
+                | seq -> rule :: (dummy, Ast.Paragraph (convert_seq (List.rev seq))) :: accum in
+            convert_paragraph accum [] tl
+        | hd :: tl ->
+            convert_paragraph accum (hd :: seq) tl
 
     and convert_frag frag = blockify (List.map convert_block frag)
 
     and convert_block = function
-        | Omd.H1 seq                 -> Block (dummy, Ast.Section (1, convert_seq seq))
-        | Omd.H2 seq                 -> Block (dummy, Ast.Section (2, convert_seq seq))
-        | Omd.H3 seq                 -> Block (dummy, Ast.Section (3, convert_seq seq))
-        | Omd.H4 seq                 -> Block (dummy, Ast.Section (4, convert_seq seq))
-        | Omd.H5 seq                 -> Block (dummy, Ast.Section (5, convert_seq seq))
-        | Omd.H6 seq                 -> Block (dummy, Ast.Section (6, convert_seq seq))
-        | Omd.Paragraph [Omd.Hr]     -> Block (dummy, Ast.Rule)
-        | Omd.Paragraph seq          -> Block (dummy, Ast.Paragraph (convert_seq seq))
-        | Omd.Ul frags               -> Block (dummy, Ast.Itemize (convert_list frags))
-        | Omd.Ol frags               -> Block (dummy, Ast.Enumerate (convert_list frags))
-        | Omd.Ulp frags              -> Block (dummy, Ast.Itemize (convert_list frags))
-        | Omd.Olp frags              -> Block (dummy, Ast.Enumerate (convert_list frags))
-        | Omd.Code_block (lang, txt) -> Block (add_style "lang" lang, Ast.Source txt)
-        | Omd.Hr                     -> Block (dummy, Ast.Rule)
-        | Omd.NL                     -> Drop
-        | Omd.Html _                 -> raise (Unsupported_feature "Html")
-        | Omd.Html_block _           -> raise (Unsupported_feature "Html_block")
-        | Omd.Html_comment _         -> raise (Unsupported_feature "Html_comment")
-        | Omd.Raw _                  -> raise (Unsupported_feature "Raw")
-        | Omd.Raw_block _            -> raise (Unsupported_feature "Raw_block")
-        | Omd.Blockquote frag        -> Block (dummy, Ast.Quote (convert_frag frag))
-        | Omd.Img (alt, src, _title) -> Block (dummy, Ast.Picture (src, alt))
+        | Omd.H1 seq ->
+            Block (dummy, Ast.Section (1, convert_seq seq))
+        | Omd.H2 seq ->
+            Block (dummy, Ast.Section (2, convert_seq seq))
+        | Omd.H3 seq ->
+            Block (dummy, Ast.Section (3, convert_seq seq))
+        | Omd.H4 seq ->
+            Block (dummy, Ast.Section (4, convert_seq seq))
+        | Omd.H5 seq ->
+            Block (dummy, Ast.Section (5, convert_seq seq))
+        | Omd.H6 seq ->
+            Block (dummy, Ast.Section (6, convert_seq seq))
+        | Omd.Paragraph xs ->
+            Multiblock (convert_paragraph [] [] xs)
+        | Omd.Ul frags ->
+            Block (dummy, Ast.Itemize (convert_list frags))
+        | Omd.Ol frags ->
+            Block (dummy, Ast.Enumerate (convert_list frags))
+        | Omd.Ulp frags ->
+            Block (dummy, Ast.Itemize (convert_list frags))
+        | Omd.Olp frags ->
+            Block (dummy, Ast.Enumerate (convert_list frags))
+        | Omd.Code_block (lang, txt) ->
+            Block (add_style "lang" lang, Ast.Source txt)
+        | Omd.Hr ->
+            Block (dummy, Ast.Rule)
+        | Omd.NL ->
+            Drop
+        | Omd.Blockquote frag ->
+            Block (dummy, Ast.Quote (convert_frag frag))
+        | Omd.Img (alt, src, _title) ->
+            Block (dummy, Ast.Picture (src, alt))
         | Omd.Img_ref (ref, link, txt, fallback) ->
             begin match ref#get_ref link with
                 | Some (href, _title) -> Block (dummy, Ast.Picture (href, txt))
                 | None                -> Block (dummy, Ast.Paragraph ([dummy, Ast.Plain fallback#to_string]))
             end
-        | Omd.X _ -> raise (Unsupported_feature "X")
-        | x -> convert_inline x
+        | (Omd.Html _ | Omd.Html_block _ | Omd.Html_comment _ | Omd.Raw _ | Omd.Raw_block _ | Omd.X _) as x ->
+            raise (Unsupported_block x)
+        | x ->
+            convert_inline x
 
     and convert_list frags =
         let aux frag = (dummy, convert_frag frag) in
@@ -145,7 +200,9 @@ let ast_from_string ~linenum_offset ~inline_extdefs ~block_extdefs str =
         `Okay (Omd.of_string str |> ast_of_omd)
     with exc ->
         let msg = match exc with
-            | Unsupported_feature x -> Printf.sprintf "The document uses an unsupported feature (%s)" x
-            | exc                   -> raise exc
+            | Unsupported_raw x    -> Printf.sprintf "Cannot handle raw '%s' in inline context" x
+            | Unsupported_block x  -> Printf.sprintf "Cannot handle '%s' in block context" (Omd.to_markdown [x])
+            | Unsupported_inline x -> Printf.sprintf "Cannot handle '%s' in inline context" (Omd.to_markdown [x])
+            | exc                  -> raise exc
         in `Error [(None, None, msg)]
 
