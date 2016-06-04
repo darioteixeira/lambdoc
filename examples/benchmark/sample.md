2fdeb26da84ab0eaf27603013c7200e7d8a994f9
darioteixeira <darioteixeira@yahoo.com>
2016-06-04 19:32:33 +0100

    Update benchmark example

---
+++ examples/benchmark/sample.md
@@ -0,0 +1,1 @@
+../markups/markdown/sample.md
--- examples/benchmark/test.ml
+++ examples/benchmark/test.ml
@@ -11,13 +11,19 @@
 (** {1 Functions and values}                                                    *)
 (********************************************************************************)
 
+let build_test (desc, ext, reader) =
+    let fname = "sample." ^ ext in
+    let src = BatIO.read_all (BatIO.input_channel (open_in fname)) in
+    ("Parsing with " ^ desc, reader, src)
+
 let () =
-    let src_lambtex = BatIO.read_all (BatIO.input_channel (open_in "sample.lambtex")) in
-    let src_lambxml = BatIO.read_all (BatIO.input_channel (open_in "sample.lambxml")) in
-    let tests =
+    let test_inputs =
         [
-        ("Parsing with Lambtex", (fun src -> Lambdoc_rlambtex_reader.Trivial.ambivalent_from_string src), src_lambtex);
-        ("Parsing with Lambxml", (fun src -> Lambdoc_rlambxml_reader.Trivial.ambivalent_from_string src), src_lambxml);
-        ]
-    in Benchmark.(throughputN ~min_count:100L 30 tests |> tabulate)
+        ("Lambtex", "tex", fun str -> Lambdoc_rlambtex_reader.Trivial.ambivalent_from_string ~options:() str);
+        ("Lambwiki", "wiki", fun str -> Lambdoc_rlamblite_reader.Trivial.ambivalent_from_string ~options:`Lambwiki str);
+        ("Lambxml", "xml", fun str -> Lambdoc_rlambxml_reader.Trivial.ambivalent_from_string ~options:() str);
+        ("Markdown", "md", fun str -> Lambdoc_rlamblite_reader.Trivial.ambivalent_from_string ~options:`Markdown str);
+        ] in
+    let tests = List.map build_test test_inputs in
+    Benchmark.(throughputN ~min_count:100L 10 tests |> tabulate)
 
