f449d184191458f7c6f444d918af3d7f846104ff
darioteixeira <darioteixeira@yahoo.com>
2016-06-11 13:28:05 +0100

    Port the benchmark example to the latest Lambdoc API

--- examples/benchmark/test.ml
+++ examples/benchmark/test.ml
@@ -6,6 +6,8 @@
 *)
 (********************************************************************************)
 
+open Lambdoc_prelude
+
 
 (********************************************************************************)
 (** {1 Functions and values}                                                    *)
@@ -13,10 +15,15 @@
 
 let build_test (desc, ext, reader) =
     let fname = "sample." ^ ext in
-    let src = BatIO.read_all (BatIO.input_channel (open_in fname)) in
+    let chan = Pervasives.open_in fname in
+    let src = Pervasives.input_all chan in
+    Pervasives.close_in chan;
     ("Parsing with " ^ desc, reader, src)
 
 let () =
+    Printf.printf "IMPORTANT NOTE: the inputs for each reader differ significantly in complexity.\n";
+    Printf.printf "Therefore, these benchmarks cannot be used to compare one reader against another.\n";
+    Printf.printf "They are nevertheless useful for comparing performance across compiler versions.\n\n";
     let test_inputs =
         [
         ("Lambtex", "tex", fun str -> Lambdoc_rlambtex_reader.Trivial.ambivalent_from_string ~options:() str);
@@ -25,5 +32,5 @@ let () =
         ("Markdown", "md", fun str -> Lambdoc_rlamblite_reader.Trivial.ambivalent_from_string ~options:`Markdown str);
         ] in
     let tests = List.map build_test test_inputs in
-    Benchmark.(throughputN ~min_count:100L 10 tests |> tabulate)
+    ignore Benchmark.(throughputN ~min_count:100L 10 tests)
 
